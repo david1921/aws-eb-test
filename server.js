c638c9ae3fbcfc06491c4e5c7bd0e7ba247d1907
David Mbura <David.Mbura@bhnetwork.com>
2018-09-13 17:27:32 -0700

    test again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const pg = require('pg');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
